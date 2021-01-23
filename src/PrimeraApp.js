db23d38f86429ca6792f0767051aed1b49884141
José Juan Patrón Guerrero <josejuanpatron1630@gmail.com>
2021-01-23 16:36:55 -0600

    Se inico la seccion

---
+++ src/PrimeraApp.js
@@ -0,0 +1,16 @@
+// FC
+// import React, { Fragment} from 'react';
+// <></> Fragments cortos
+
+import React from 'react';
+
+const PrimeraApp = () => {
+
+    return (
+      <>
+      <h1> Hola mundo </h1>
+      <p> Mi primera app </p>
+      </>
+    )
+}
+export default PrimeraApp;
