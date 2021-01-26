00d9fdc6e641baf364ebe3a9a5b26b3a2d8ac450
José Juan Patrón Guerrero <josejuanpatron1630@gmail.com>
2021-01-25 22:54:38 -0600

    Se vieron los hooks, el state y el setSate

--- src/counter/CounterApp.js
+++ src/counter/CounterApp.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes  from 'prop-types'
 
 // funcional component
 
-const CounterApp = ({value } ) => {
-  const handleadd = (event)=>  {
-    console.log(event);
-    return 1;
+const CounterApp = ({value  = 2} ) => {
+
+  const [counter, setCounter] = useState( value );
+// retorn un arreglo
+  const handleadd = ()=>  {
+    // console.log(event);
+
+    // setCounter(  counter + 1);
+    setCounter( (c) => c +1 );
+    // estas son las 2 maneras de manejar el counter y su estado
+    // cambiamos el valor del counter
+    // solo las funciones que estan dedicadas a cambiar el state
   }
+
+  const handleRest = () => {
+    setCounter( c => c - 1 )
+  }
+
+  const handleReste = () => {
+      setCounter( value );
+  }
+
 
   return (
     <>
     <h1> Counter App </h1>
-    <h2>  { value } </h2>
+    <h2>  { counter } </h2>
+
     <button  onClick={ handleadd } > +1 </button>
-    </>
+    <button  onClick={ handleRest } > -1 </button>
+    <button  onClick={ handleReste } > RESET </button>
+
+  </>
   )
 }
 
