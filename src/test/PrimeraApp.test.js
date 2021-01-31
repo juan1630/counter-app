eacf7cf1c598ae75c831aa96fa9c3aee5416181f
José Juan Patrón Guerrero <josejuanpatron1630@gmail.com>
2021-01-31 09:05:37 -0600

    Se agregaron mas pruebas

--- src/test/PrimeraApp.test.js
+++ src/test/PrimeraApp.test.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react'
+
+import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
 import PrimeraApp from '../PrimeraApp';
 
+
+
 describe('preubas en el componente primer app', () => {
-    test('Debe mostrar hola soy goku', () => {
-        const saludo ="Hola soy Gokú";
-        // cuando son porps requeridas se deb de enviar dentro del componente
-        const { getByText } = render( <PrimeraApp  saludo={saludo} /> );
-        // wrapper.getByText();
-        expect( getByText( saludo ) ).toBeInTheDocument();
+
+    // test('Debe mostrar hola soy goku', () => {
+    //     const saludo ="Hola soy Gokú";
+    //     // cuando son porps requeridas se deb de enviar dentro del componente
+    //     const { getByText } = render( <PrimeraApp  saludo={saludo} /> );
+    //     // wrapper.getByText();
+    //     expect( getByText( saludo ) ).toBeInTheDocument();
+    // });
+
+
+    test('debe de mostrar el componente <PrimeraApp/>', ()=> {
+
+          const saludo = "Hola soy Gokú";
+          const wrapper  = shallow( <PrimeraApp saludo={saludo}/> );
+          expect(  wrapper ).toMatchSnapshot();
+          //
     });
+
+// fin del describe
 });
